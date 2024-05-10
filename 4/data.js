a3c57e6cc8ea0f7ca8a781d2e2b7643ac7a96850
SHAHZAIB <shahzaibintzar27346@gmail.com>
2024-05-10 09:13:24 +0500

    working API

---
+++ 4/data.js
@@ -0,0 +1,10 @@
+const data =[
+    {name:'shahzaib', email:'shahzab@gmail.com'},
+    {name:'subhan', email:'subhan@gmail.com'},
+    {name:'sami', email:'sami@gmail.com'},
+    {name:'waleed', email:'waleed@gmail.com'},
+    {name:'imtiaz', email:'imtiaz@gmail.com'},
+    {name:'ali', email:'ali@gmail.com'},
+    {name:'ahmed', email:'ahmed@gmail.com'},
+]
+module.exports=data;
--- 4/index.js
+++ 4/index.js
@@ -1,2 +1,8 @@
 const colors = require("colors");
-console.log('first'.red.bgCyan)
+console.log('shahzaib'.red.bgCyan)
+// import chalk from "chalk";
+
+
+
+// console.log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+// console.log(chalk.blue.bgRed.bold('Hello world!'));
