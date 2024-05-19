0952177012be69d1caa01b18dccdeed75fea7c5c
shahzaibintzar <shahzaibintzar27346@gmail.com>
2024-05-19 20:39:03 +0500

    working react.js Node.js

---
+++ e-dashbord/client/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import Navbar from './navbar'
+import './App.css';
+import { BrowserRouter, Routes , Route} from 'react-router-dom';
+
+
+function App() {
+  return (
+   <div>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<h1>Products Page</h1>} />
+      <Route path="/add" element={<h1>Add Products Page</h1>} />
+      <Route path="/update" element={<h1>Update Products Page</h1>} />
+      <Route path="/logout" element={<h1>Logout Products Page</h1>} />
+      <Route path="/profile" element={<h1>Profile Products Page</h1>} />
+    </Routes>
+    </BrowserRouter>
+   </div>
+  );
+}
+
+export default App;
---
+++ e-dashbord/client/src/navbar.js
@@ -0,0 +1,18 @@
+import react from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+export default  function Nav (){
+    return(
+        <nav>
+            <ul className='nav-ul'>
+                <li><Link to='/'>Products</Link></li>
+                <li><Link to='/add'>Add Products</Link></li>
+                <li><Link to='/update'>Updtae Products</Link></li>
+                <li><Link to='/logout'>Logout</Link></li>
+                <li><Link to='/profile'>Profile</Link></li>
+            </ul>
+        </nav>
+    )
+}
