d8bc99cde8f0a213362d23da2520687a9f76415b
shahzaibintzar <shahzaibintzar27346@gmail.com>
2024-05-20 14:45:45 +0500

    MongoDB connect in seoarate config file complete

--- e-dashbord/server/index.js
+++ e-dashbord/server/index.js
@@ -1,12 +1,39 @@
+
+
+//                                 simple data get mongoDb in one file
+
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const app = express();
+
+// require("dotenv").config()
+
+// const connectDB = async ()=>{
+// mongoose.connect(process.env.DATABASE);
+// const productsSchema = new mongoose.Schema({});
+// const productsModel =  mongoose.model('users', productsSchema);
+// const data = await productsModel.find();
+// console.log(data);
+// }
+// connectDB();
+// app.listen(4000)
+
+
+
+//                                   MongoDB connect server using config file
+
+
+
+
 const express = require('express');
-const mongoose = require('mongoose');
+const User = require('./db/user');
+require('./db/config');
 const app = express();
-const connectDB = async ()=>{
-mongoose.connect('mongodb+srv://TrelloClone:00998877@cluster0.0git2uy.mongodb.net/UserData?retryWrites=true&w=majority&appName=Cluster0');
-const productsSchema = new mongoose.Schema({});
-const productsModel =  mongoose.model('users', productsSchema);
-const data = await productsModel.find();
-console.log(data);
-}
-connectDB();
+
+app.use(express.json());
+app.post("/register",async (req,res)=>{
+  const user = new User(req.body)
+  const result = await user.save();
+res.send(result);
+})
 app.listen(4000)
