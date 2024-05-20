4b79a0a42d95a57983787003fb7209b5b1c3992c
shahzaibintzar <shahzaibintzar27346@gmail.com>
2024-05-20 13:09:09 +0500

    simple mongoDB data get

--- e-dashbord/server/index.js
+++ e-dashbord/server/index.js
@@ -1,22 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
-
 const app = express();
-
 const connectDB = async ()=>{
-  mongoose.connect('mongodb://localhost:27017/userData');
-
-  const productsSchema =new mongoose.Schema({});
-
+mongoose.connect('mongodb+srv://TrelloClone:00998877@cluster0.0git2uy.mongodb.net/UserData?retryWrites=true&w=majority&appName=Cluster0');
+const productsSchema = new mongoose.Schema({});
 const productsModel =  mongoose.model('users', productsSchema);
 const data = await productsModel.find();
-
 console.log(data);
-
 }
-
-
 connectDB();
-
 app.listen(4000)
