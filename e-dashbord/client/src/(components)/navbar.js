e489d81816eded4b1e743924c6b7e26d4ec37c85
shahzaibintzar <shahzaibintzar27346@gmail.com>
2024-05-21 15:01:08 +0500

    Navber changing

--- e-dashbord/client/src/(components)/navbar.js
+++ e-dashbord/client/src/(components)/navbar.js
@@ -1,28 +1,49 @@
-import { Link ,useNavigate} from 'react-router-dom';
-export default  function Nav (){
-    const auth = localStorage.getItem('user');
-    const navigate = useNavigate();
-    const logout = ()=>{
-        localStorage.clear();
-        navigate('/signup')
-    }
-    return(
-        <nav>
-            <ul className='nav-ul'>
-                <li><Link to='/'>Products</Link></li>
-                <li><Link to='/add'>Add Products</Link></li>
-                <li><Link to='/update'>Updtae Products</Link></li>
-                <li><Link to='/profile'>Profile</Link></li>
-                {
-                    auth?
-                    <li><Link onClick={logout} to='/signup'>Logout</Link></li>
-                    :
-                    <>
-                    <li><Link to='/login'>Login</Link></li>
-                    <li><Link to='/signup'>SignUp</Link></li>
-                    </>
-                }
-            </ul>
-        </nav>
-    )
+import { Link, useNavigate } from "react-router-dom";
+
+export default function Nav() {
+  const auth = localStorage.getItem("user");
+  const navigate = useNavigate();
+  const logout = () => {
+    localStorage.clear();
+    navigate("/signup");
+  };
+  return (
+    <div>
+    <img src='https://freevector-images.s3.amazonaws.com/uploads/vector/preview/36682/36682.png' 
+    alt='logo'
+    className="logo"
+     />
+      {auth ? (
+        <ul className="nav-ul">
+          <li>
+            <Link to="/">Products</Link>
+          </li>
+          <li>
+            <Link to="/add">Add Products</Link>
+          </li>
+          <li>
+            <Link to="/update">Updtae Products</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link onClick={logout} to="/signup">
+              Logout
+              ({JSON.parse(auth).name})
+            </Link>
+          </li>
+        </ul>
+      ) : (
+        <ul className="nav-ul nav-right">
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/signup">SignUp</Link>
+          </li>
+        </ul>
+      )}
+    </div>
+  );
 }
